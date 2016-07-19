5dc6fc0b6829289654d88ef7e956cf0d5196af03
Jiang Chen <criver@gmail.com>
2016-07-19 05:13:02 +0000

    Update the version to 0.20

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.17',
+    version='0.20',
 
     description='GBDT',
     long_description=long_description,
