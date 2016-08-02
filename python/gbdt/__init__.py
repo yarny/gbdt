22434007b6ec30e4cb67fa879e33bde24fec5147
Jiang Chen <jiang.chen@airbnb.com>
2016-08-02 00:06:07 -0700

    Added OsX support.

--- python/gbdt/__init__.py
+++ python/gbdt/__init__.py
@@ -1,6 +1,12 @@
+import os
+import sys
+abspath = os.path.dirname(os.path.abspath(__file__))
+so_lib_path = 'darwin_x86_64' if sys.platform == 'darwin' else 'linux_x86_64'
+sys.path.append('{0}/lib/{1}'.format(abspath, so_lib_path))
+
 from ._version import version_info, __version__
-from ._gbdt import DataStore
+from libgbdt import DataStore
+from libgbdt import Forest
+from libgbdt import init_logging
 from ._gbdt import train
-from ._gbdt import Forest
-from ._gbdt import init_logging
 from ._forest_visualizer import ForestVisualizer
