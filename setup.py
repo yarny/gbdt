ba2c547b5c4579a2f207dcb096663390305bf09e
Jiang Chen <jiang.chen@airbnb.com>
2016-09-25 03:45:49 -0700

    Make stable release.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ When is GBDT good for you?
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX :: Linux',
