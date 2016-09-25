71141859c5a3c5340b7490e38eddf0a2e32260bd
Jiang Chen <jiang.chen@airbnb.com>
2016-09-25 02:57:06 -0700

    Upload to pypi

--- python/gbdt/_version.py
+++ python/gbdt/_version.py
@@ -1,2 +1,2 @@
-version_info = (0, 22, 'beta')
+version_info = (0, 22, 'b1')
 __version__ = '.'.join(map(str, version_info))
--- setup.py
+++ setup.py
@@ -25,8 +25,21 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     version=_version.__version__,
 
-    description='GBDT',
-    long_description=long_description,
+    description='High performance implementation of GBDT family of algorithm',
+    long_description="""GBDT is a high performance and full featured C++ implementation of [Jerome H. Friedman's Gradient Boosting Decision Trees Algorithm](http://statweb.stanford.edu/~jhf/ftp/stobst.pdf) and its modern offsprings,. It features high efficiency, low memory footprint, collections of loss functions and built-in mechanisms to handle categorical features and missing values.
+
+When is GBDT good for you?
+-----------
+* **You are looking beyond linear models.**
+  * Gradient Boosting Decision Trees Algorithms is one of the best offshelf ML algorithms with built-in capabilities of non-linear transformation and feature crossing.
+* **Your data is too big to load into memory with existing ML packages.**
+  * GBDT reduces memory footprint dramatically with feature bucketization. For some tested datasets, it used 1/7 of the memory of its counterpart and took only 1/2 time to train. See [docs/PERFORMANCE_BENCHMARK.md](https://github.com/yarny/gbdt/blob/master/docs/PERFORMANCE_BENCHMARK.md) for more details.
+* **You want better handling of categorical features and missing values.**
+  * GBDT has built-in mechanisms to figure out how to split categorical features and place missing values in the trees.
+* **You want to try different loss functions.**
+  * GBDT implements various pointwise, pairwise, listingwis loss functions including mse, logloss, huberized hinge loss, pairwise logloss,
+[GBRank](http://www.cc.gatech.edu/~zha/papers/fp086-zheng.pdf) and [LambdaMart](https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/MSR-TR-2010-82.pdf). It supports easily addition of your own custom loss functions.
+    """,
 
     # The project's main homepage.
     url='https://github.com/yarny/gbdt',
@@ -40,21 +53,16 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Machine Learning Practitioners',
-        'Topic :: Machine Learning',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: Apache 2.0',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: C++',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7'
